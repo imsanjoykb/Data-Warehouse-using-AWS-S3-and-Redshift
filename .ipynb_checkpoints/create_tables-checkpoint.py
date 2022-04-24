a0a021754fd5c0d8f3d359ef17f1b059e1f1e2c9
imsanjoykb <sanjoy.eee32@gmail.com>
2022-04-24 23:07:07 +0600

    Program file add

---
+++ .ipynb_checkpoints/create_tables-checkpoint.py
@@ -0,0 +1,38 @@
+import configparser
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+
+def drop_tables(cur, conn):
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def create_tables(cur, conn):
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    print('Connecting to redshift')
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    print('Connected to redshift')
+    cur = conn.cursor()
+
+    print('Dropping existing tables if any')
+    drop_tables(cur, conn)
+    
+    print('Creating tables')
+    create_tables(cur, conn)
+
+    conn.close()
+    print('Create table Ended')
+
+
+if __name__ == "__main__":
+    main()
